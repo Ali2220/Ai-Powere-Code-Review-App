b69c2065f6885441bdb6ddba76a39f81bf3bb968
Ali2220 <alisarwar0277@gmail.com>
2025-03-10 11:44:56 +0500

    Update app.js
    update app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -6,16 +6,16 @@ const aiRoutes = require('./routes/ai.routes')   // AI related routes import kar
 
 app.use(cors())
 app.use(express.json())
-// AI routes ko /ai endpoint pe connect kiya
-// Yaha pe hum ai.routes.js ke saare routes ko /ai ke neeche attach kar rahe hai.
-// Matlab, ab URL banega:  http://localhost:3000/ai/get-response
+
 
 
 app.get('/', (req, res) => {
     res.send('hello world');
 })
 
-
+// AI routes ko /ai endpoint pe connect kiya
+// Yaha pe hum ai.routes.js ke saare routes ko /ai ke neeche attach kar rahe hai.
+// Matlab, ab URL banega:  http://localhost:3000/ai/get-response
 app.use('/ai', aiRoutes)
 
 module.exports = app
